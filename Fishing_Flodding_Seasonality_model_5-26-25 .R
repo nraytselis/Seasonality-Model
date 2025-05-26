141b3db8b2679d230ca878ff13d537bb1a567579
nraytselis <98825564+nraytselis@users.noreply.github.com>
2025-05-26 16:38:24 -0400

    Add files via upload

--- Fishing_Flodding_Seasonality_model_5-26-25 .R
+++ Fishing_Flodding_Seasonality_model_5-26-25 .R
@@ -10,7 +10,7 @@ library(deSolve)
 Pond_ODE =function(t, y, parameters) {
   
   with(as.list(parameters),{
-    N=y[1]; J=y[2]; A=y[3]; Es = y[4:(4+latent_stages - 1)]; I = y[4+latent_stages]; L3F = y[5]
+    N=y[1]; J=y[2]; A=y[3]; Es = y[4:(4+latent_stages - 1)]; I = y[4+latent_stages]; Preds = y[5]; L3F = y[6]
     VOL = 1
     
     Pred_A = f*Preds/(1 + f*h*(A + f_J*h_J*J + f_N*h_N*N)/VOL  + i_P*max(Preds-1, 0)/VOL)
@@ -40,9 +40,11 @@ Pond_ODE =function(t, y, parameters) {
     
     dIdt = as.numeric(latent_progression[latent_stages]) - d_A_c*I - Pred_A*I
     
-    dL3Fdt = Pred_N*I - d_W*L3F - d_F*L3F
+    dPredsdt = Pred_N*N + Pred_J*J + Pred_A*A + Pred_A*I - d_F*Preds #NEED TO add exposed classes, right now throwing error 
     
-    result = c(dNdt,dJdt,dAdt, dEsdt, dIdt,dL3Fdt)
+    dL3Fdt = Pred_A*I - d_W*L3F - d_F*L3F
+    
+    result = c(dNdt,dJdt,dAdt, dEsdt, dIdt,dPredsdt, dL3Fdt)
     
     return(list(result))
   }
@@ -61,13 +63,12 @@ parameters["can"] = 0
 parameters["f"] = 0.004
 parameters["f_N"] = 4.704984e-02
 parameters["f_J"] = 3.471900e-02
-parameters["h"] = 3.179536e-03
+parameters["h"] = 0.003
 parameters["h_N"] = 4.675860e-01
 parameters["h_J"] = 3.951459e-01
 parameters["i_P"] = 1.911824e-01
 parameters["d_W"] = 0.001
 parameters["d_F"] = 0.0001
-parameters["Preds"] = 1
 
 parameters = unlist(parameters)
 
@@ -76,27 +77,44 @@ Exposed_values = rep(0, times=parameters["latent_stages"])
 names(Exposed_values) = Exposed_names
 Exposed_values
 
-Initial_conditions = c(N = 7500, J = 6000, A = 700, Exposed_values, I = 0, L3F = 0)/15
+Initial_conditions = c(N = 7500, J = 6000, A = 700, Exposed_values, I = 0, L3F = 0, Preds = 1)/15
 timespan = 365
 
-# dose in the L1s
-introduction_times = numeric()
-introduction_times = (1:60)*30
-event_data = data.frame(var = "L1", time = introduction_times, value = 1000000, method = "add")
+#fish added during rainy season
+introduction_times_fish = numeric()
+introduction_times_fish = c(1:180, 270:365) 
+event_data_fish = data.frame(var = "Preds", time = introduction_times_fish, value = 10, method = "add")
 
+#fishing during dry season
+introduction_times_fishing = numeric()
+introduction_times_fishing = c(181:269)
+event_data_fishing = data.frame(var = "Preds", time = introduction_times_fishing, value = -10, method = "add")
 
+#combined list of events
+all_events = rbind(event_data_fish, event_data_fishing)
+
+#run simulation
 PondSim = data.frame(ode(y = Initial_conditions, times=1:timespan, parms=parameters, hmax=1,
-                            method="lsoda", func=Pond_ODE, events = list(event_data)))
+                            method="lsoda", func=Pond_ODE, events = list(data = all_events))) 
 
+#reformat sim data frame
 PondSim[,"Es"] = rowSums(PondSim) - PondSim[,"N"] - PondSim[,"J"]- PondSim[,"A"] - PondSim[,"I"] - PondSim[,"time"]
-
-PondSim = data.frame(ode(y = Initial_conditions, times=1:timespan, parms=parameters, hmax=1,
-                            method="lsoda", func=Pond_ODE))
-
 PondSim[,"Exposed"] = apply(X=PondSim[,which(str_detect(colnames(PondSim),"E"))],MARGIN=1,FUN = sum) 
-PondSim = PondSim %>% select(time,N,J,A,Exposed,I,L3F)
-PondSim = PondSim %>% pivot_longer(cols = c(N,J,A,Exposed,I,L3F)) 
 
+#check to make sure that events worked, NEED TO CHANGE EVENT SO THAT CANNOT GO NEGATIVE 
+ggplot(PondSim, aes(x = time)) +
+  geom_line(aes(y = Preds), color = "blue") +
+  geom_line(aes(y = L3F), color = "red") +
+  labs(y = "Fish (blue = total, red = infected)", title = "Predator Fish Dynamics Over Time") +
+  theme_minimal()
+
+#reformat for plotting
+PondSim = PondSim %>% select(time,N,J,A,Exposed,I,Preds, L3F)
+PondSim = PondSim %>% pivot_longer(cols = c(N,J,A,Exposed,I,Preds, L3F)) 
+
+#plot sim 
 p1 = ggplot(PondSim, aes(x=time, y = value + 0.01 , group = name, color = name)) + 
-  geom_line() + ylab("density per L") + theme_minimal() + scale_x_log10()
+  geom_line() + ylab("density per L") + theme_minimal() 
 p1
+
+#need to make sure L3Fs cannot go negative, as they currently can
